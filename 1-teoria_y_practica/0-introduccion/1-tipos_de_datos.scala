96d2ad70dba4cc92bf2a4577b2885da5729b1fce
Alfonso Perez <perezlino@gmail.com>
2023-02-16 21:44:39 -0300

    Actualizacion directorio teoria y practica - 0-introduccion

---
+++ 1-teoria_y_practica/0-introduccion/1-tipos_de_datos.scala
@@ -0,0 +1,23 @@
+/*  ======================================================
+    ================ TIPOS DE DATOS SCALA ================
+    ======================================================
+
+     _____________________________________________________________________________________________________________
+    |   Tipos de datos  |                            Descripción                                                  |              
+    |-------------------|-----------------------------------------------------------------------------------------|  
+    |   Byte            |   8 bits con signo.Rango de valores entre -128 a la 127                                 |  
+    |   Short           |   16 bits con signo. Rango de valores -32.768-32767                                     |  
+    |   Int             |   De 32 bits con signo. Rango de valores desde -2147483648 hasta 2147483647             |  
+    |   Long            |   De 64 bits con signo o. intervalo numérico -9223372036854775808-9223372036854775807   |  
+    |   Float           |   32-bit de punto flotante de precisión simple IEEE754                                  |  
+    |   Double          |   64-bit de punto flotante de precisión simple IEEE754                                  |  
+    |   Char            |   16 sin signo de caracteres Unicode. Rango de valores U + 0000 a U + FFFF              |  
+    |   String          |   Secuencia de caracteres                                                               |      
+    |   Boolean         |   Verdadero o falso                                                                     |  
+    |   Unit            |   Corresponde a “ningún valor”                                                          |  
+    |   Null            |   Referencias nulas o vacías                                                            |  
+    |   Nothing         |   Subtipo de cualquier otro tipo                                                        |  
+    |   Any             |   El supertipo de cualquier tipo. Cualquier objeto es de tipo Any                       |  
+    |   AnyRef          |   El supertipo de cualquier tipo “referencia                                            |      
+    |-------------------|-----------------------------------------------------------------------------------------|
+*/
